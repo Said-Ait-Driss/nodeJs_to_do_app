118fb7223e279ec42aeaa2f43baf8aa1936d05c9
Said Ait Driss <71972356+Said-Ait-Driss@users.noreply.github.com>
2022-01-06 19:56:52 +0100

    base on mvc stracture and create todoController

---
+++ controllers/todoController.js
@@ -0,0 +1,17 @@
+module.exports = function (app){
+
+    // for get todo
+    app.get("/todo",function(req,res){
+
+    });
+
+    // for add todo
+    app.post("/post",function(req,res){
+
+    });
+
+    // for delete todo
+    app.delete("/post",function(req,res){
+
+    });
+};
